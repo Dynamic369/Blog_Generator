2c7415ff035e2ecb47b2cc8e402283e01478565f
Pradum Gupta <guptapradum684@gmail.com>
2025-10-18 22:33:42 +0530

    langauge

--- src/nodes/blog_node.py
+++ src/nodes/blog_node.py
@@ -1,4 +1,6 @@
 from src.states.blogstate import BlogState
+from langchain_core.messages import HumanMessage,SystemMessage
+from src.states.blogstate import Blog
 
 
 class BlogNode:
@@ -38,3 +40,38 @@ class BlogNode:
             response = self.llm.invoke(system_message)
             
             return {"blog": {"title":state['blog']['title'], "content":response.content}}
+        
+    def translation(self, state:BlogState):
+        """
+        Translate the content into specified language.
+        """
+        translation_prompt = """ 
+                            Translate the following content into {current_language}.
+                            - Maintain the original tone, style, and formatting.
+                            - Adapt cultural references and idoms to be appropiate for {current_language}
+
+                            ORIGINAL CONTENT
+                            {blog_content}
+                            """
+        blog_content = state['blog']['content']
+        messages =[
+            HumanMessage(translation_prompt.format(current_language=state['current_language'], blog_content=blog_content))
+        ]
+
+        translation_content = self.llm.with_structured_output(Blog).invoke(messages)
+        return {"blog": {"content": translation_content}}
+    
+    
+    def route(self, state:BlogState):
+        return {"current_language":state['current_language']}
+    
+    def route_decision(self,state:BlogState):
+        """
+        Route the content to the respective translation function.
+        """
+        if state["current_language"] == "hindi":
+            return "hindi"
+        elif state["current_language"] == "french": 
+            return "french"
+        else:
+            return state['current_language']
